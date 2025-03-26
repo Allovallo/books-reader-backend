17691e38fd982060ae563dec7525958768b40b69
Allovallo <grossco@meta.ua>
2025-03-26 13:48:12 +0200

    port updated

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const booksRouter = require("./routes/api/books");
 
 const app = express();
+// const port = process.env.PORT || 4000;
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 5000;
 
 const DB_HOST =
   "mongodb+srv://grossco:poiP36Z7WnTvbTLJ@cluster0.2qtkt.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0";
@@ -13,6 +13,7 @@ mongoose
   .then(() => {
     app.listen(port);
     console.log("Successfully connected!");
+    console.log(`Example app listening on port ${port}`);
   })
   .catch((error) => {
     console.log(error.message);
