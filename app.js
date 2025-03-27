54118016c9d034b198eb3492a7c56d85d8ecf51c
Allovallo <grossco@meta.ua>
2025-03-27 09:58:52 +0200

    app.js updated

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ module.exports = app;
 const mongoose = require("mongoose");
 
 // const app = require("./app");
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 const DB_HOST =
   "mongodb+srv://grossco:poiP36Z7WnTvbTLJ@cluster0.2qtkt.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0";
@@ -38,9 +38,9 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(port);
+    app.listen(5000);
     console.log("Successfully connected!");
-    console.log(`Example app listening on port ${port}`);
+    // console.log(`Example app listening on port ${port}`);
   })
   .catch((error) => {
     console.log(error.message);
