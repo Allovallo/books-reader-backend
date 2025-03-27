5b34cdafb08e00c4e1a6f5e9170d839fde7987c6
Allovallo <grossco@meta.ua>
2025-03-27 09:47:16 +0200

    app.js updated

--- app.js
+++ app.js
@@ -24,3 +24,25 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+const mongoose = require("mongoose");
+
+// const app = require("./app");
+const port = process.env.PORT || 5000;
+
+const DB_HOST =
+  "mongodb+srv://grossco:poiP36Z7WnTvbTLJ@cluster0.2qtkt.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(port);
+    console.log("Successfully connected!");
+    console.log(`Example app listening on port ${port}`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const app = require("./app");
-const port = process.env.PORT || 5000;
+// const app = require("./app");
+// const port = process.env.PORT || 5000;
 
-const DB_HOST =
-  "mongodb+srv://grossco:poiP36Z7WnTvbTLJ@cluster0.2qtkt.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0";
+// const DB_HOST =
+//   "mongodb+srv://grossco:poiP36Z7WnTvbTLJ@cluster0.2qtkt.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(port);
-    console.log("Successfully connected!");
-    console.log(`Example app listening on port ${port}`);
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(port);
+//     console.log("Successfully connected!");
+//     console.log(`Example app listening on port ${port}`);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
