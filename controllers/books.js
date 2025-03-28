c593c02e93573c240719d1fb6f12f00c73c80373
Allovallo <grossco@meta.ua>
2025-03-28 18:23:42 +0200

    files updated

--- controllers/books.js
+++ controllers/books.js
@@ -1,54 +1,54 @@
-const books = require("../models/books");
+const Book = require("../models/book");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (_, res) => {
-  const result = await books.getAll();
+  const result = await Book.find();
   res.json(result);
 };
 
-const getById = async (req, res) => {
-  const { id } = req.params;
-  const result = await books.getById(id);
+// const getById = async (req, res) => {
+//   const { id } = req.params;
+//   const result = await books.getById(id);
 
-  if (!result) {
-    throw HttpError(404, "NOT FOUND");
-  }
+//   if (!result) {
+//     throw HttpError(404, "NOT FOUND");
+//   }
 
-  res.json(result);
-};
+//   res.json(result);
+// };
 
 const add = async (req, res) => {
-  const result = await books.add(req.body);
+  const result = await Book.create(req.body);
   res.status(201).json(result);
 };
 
-const updateById = async (req, res) => {
-  const { id } = req.params;
-  const result = await books.updateById(id, req.body);
+// const updateById = async (req, res) => {
+//   const { id } = req.params;
+//   const result = await books.updateById(id, req.body);
 
-  if (!result) {
-    throw HttpError(404, "Not found!!!");
-  }
+//   if (!result) {
+//     throw HttpError(404, "Not found!!!");
+//   }
 
-  res.json(result);
-};
+//   res.json(result);
+// };
 
-const deleteById = async (req, res) => {
-  const { id } = req.params;
-  const result = await books.deleteById(id);
+// const deleteById = async (req, res) => {
+//   const { id } = req.params;
+//   const result = await books.deleteById(id);
 
-  if (!result) {
-    throw HttpError(404, "Not found!!!");
-  }
+//   if (!result) {
+//     throw HttpError(404, "Not found!!!");
+//   }
 
-  res.json({ message: "Delete success!" });
-};
+//   res.json({ message: "Delete success!" });
+// };
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
-  getById: ctrlWrapper(getById),
+  // getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
-  updateById: ctrlWrapper(updateById),
-  deleteById: ctrlWrapper(deleteById),
+  // updateById: ctrlWrapper(updateById),
+  // deleteById: ctrlWrapper(deleteById),
 };
--- routes/api/books.js
+++ routes/api/books.js
@@ -9,9 +9,9 @@ const schemas = require("../../schemas/books");
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
-router.get("/:id", ctrl.getById);
+// router.get("/:id", ctrl.getById);
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
-router.delete("/:id", ctrl.deleteById);
+// router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
+// router.delete("/:id", ctrl.deleteById);
 
 module.exports = router;
