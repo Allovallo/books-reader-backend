6ff4bb5078d360e19b4f7c92b33b901a23b52798
Allovallo <grossco@meta.ua>
2025-03-28 15:08:51 +0200

    files updated

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(port);
+    app.listen(PORT);
     console.log("Successfully connected!");
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${PORT}`);
   })
   .catch((error) => {
     console.log(error.message);
