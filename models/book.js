59094ca97c0e5996e98cc72bed25c66822e9bc3d
Allovallo <grossco@meta.ua>
2025-03-29 16:56:53 +0200

    files updated

--- models/book.js
+++ models/book.js
@@ -1,8 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const bookSchema = new Schema({
-  title: String,
-  author: String,
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  favorite: { type: Boolean, default: false },
+  genre: { type: String, enum: ["sci-fi", "romantic"], required: true },
 });
 
 const Book = model("book", bookSchema);
