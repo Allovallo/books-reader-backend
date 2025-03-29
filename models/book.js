ab58eadf6ade9bb94b82bd0583fe9c656389d0e3
Allovallo <grossco@meta.ua>
2025-03-29 18:03:36 +0200

    files updated

--- models/book.js
+++ models/book.js
@@ -5,6 +5,8 @@ const bookSchema = new Schema({
   author: { type: String, required: true },
   favorite: { type: Boolean, default: false },
   genre: { type: String, enum: ["sci-fi", "romantic"], required: true },
+  // 16-10-2009
+  date: { type: String, match: /^\d{2}-\d{2}-\d{4}$/, required: true },
 });
 
 const Book = model("book", bookSchema);
--- schemas/books.js
+++ schemas/books.js
@@ -3,6 +3,8 @@ const Joi = require("joi");
 const addSchema = Joi.object({
   title: Joi.string().required(),
   author: Joi.string().required(),
+  genre: Joi.string().required(),
+  date: Joi.string().required(),
 });
 
 module.exports = { addSchema };
