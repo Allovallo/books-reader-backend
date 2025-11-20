b99a0d745f37871d82620ec35b6312f269c52d03
grossco <30566134+Allovallo@users.noreply.github.com>
2025-11-20 17:02:08 +0200

    server.js updated

--- server.js
+++ server.js
@@ -1,34 +1,18 @@
-// const mongoose = require("mongoose");
-
-// const app = require("./app");
-
-// const { DB_HOST, PORT = 3000 } = process.env;
-
-// mongoose.set("strictQuery", true);
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     app.listen(PORT);
-//     console.log("Successfully connected!");
-//     console.log(`Example app listening on port ${PORT}`);
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
-
 const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const {DB_HOST} = process.env
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(3000))
+  .then(() => {
+    app.listen(PORT);
+    console.log("Successfully connected!");
+    console.log(`Example app is listening on port ${PORT}`);
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
