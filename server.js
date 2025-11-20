b8c28c81828d3d8c07a7fba9540b41cc100a5835
grossco <30566134+Allovallo@users.noreply.github.com>
2025-11-20 15:33:50 +0200

    files updated

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => app.listen(3000))
-  .catch((error) => {console.log(error.message); process.exit(1)});
-
-// app.listen(3000, () => {
-//   console.log("Server`s running. Use our API on port: 3000");
-// });
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
