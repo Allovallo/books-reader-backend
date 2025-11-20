a5adcd0e84d3c061b93bdb7f7943b0b7279c5080
grossco <30566134+Allovallo@users.noreply.github.com>
2025-11-20 12:03:51 +0200

    files updated

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
+// const mongoose = require("mongoose");
+
+// const app = require("./app");
+
+// const { DB_HOST, PORT = 3000 } = process.env;
+
+// mongoose.set("strictQuery", true);
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(PORT);
+//     console.log("Successfully connected!");
+//     console.log(`Example app listening on port ${PORT}`);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
+
 const mongoose = require("mongoose");
 
 const app = require("./app");
-// const port = process.env.PORT || 5000;
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const DB_HOST =
+  "mongodb+srv://grossco:poiP36Z7WnTvbTLJ@cluster0.2qtkt.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT);
-    console.log("Successfully connected!");
-    console.log(`Example app listening on port ${PORT}`);
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+  .then(() => app.listen(3000))
+  .catch((error) => {console.log(error.message); process.exit(1)});
+
+// app.listen(3000, () => {
+//   console.log("Server`s running. Use our API on port: 3000");
+// });
