30e530335782a0da810759836ad2275ee4475df9
grossco <30566134+Allovallo@users.noreply.github.com>
2025-11-21 17:59:42 +0200

    files updated

--- models/book.js
+++ models/book.js
@@ -1,36 +1,47 @@
+// const { Schema, model } = require("mongoose");
+// const Joi = require("joi");
+
+// const genreList = ["sci-fi", "romantic"];
+// const dateRegexp = /^\d{2}-\d{2}-\d{4}$/;
+
+// const { handleMongooseError } = require("../helpers");
+
+// const bookSchema = new Schema(
+//   {
+//     title: { type: String, required: true },
+//     author: { type: String, required: true },
+//     favorite: { type: Boolean, default: false },
+//     genre: { type: String, enum: genreList, required: true },
+//     date: { type: String, match: dateRegexp, required: true },
+//   },
+//   { versionKey: false, timestamps: true }
+// );
+
+// bookSchema.post("save", handleMongooseError);
+
+// const addSchema = Joi.object({
+//   title: Joi.string().required(),
+//   author: Joi.string().required(),
+//   favorite: Joi.boolean(),
+//   genre: Joi.string()
+//     .valid(...genreList)
+//     .required(),
+//   date: Joi.string().pattern(dateRegexp).required(),
+// });
+
+// const schemas = { addSchema };
+
+// const Book = model("book", bookSchema);
+
+// module.exports = { Book, schemas };
+
 const { Schema, model } = require("mongoose");
-const Joi = require("joi");
 
-const genreList = ["sci-fi", "romantic"];
-const dateRegexp = /^\d{2}-\d{2}-\d{4}$/;
-
-const { handleMongooseError } = require("../helpers");
-
-const bookSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    favorite: { type: Boolean, default: false },
-    genre: { type: String, enum: genreList, required: true },
-    date: { type: String, match: dateRegexp, required: true },
-  },
-  { versionKey: false, timestamps: true }
-);
-
-bookSchema.post("save", handleMongooseError);
-
-const addSchema = Joi.object({
-  title: Joi.string().required(),
-  author: Joi.string().required(),
-  favorite: Joi.boolean(),
-  genre: Joi.string()
-    .valid(...genreList)
-    .required(),
-  date: Joi.string().pattern(dateRegexp).required(),
+const bookSchema = new Schema({
+  title: String,
+  author: String,
 });
-
-const schemas = { addSchema };
 
 const Book = model("book", bookSchema);
 
-module.exports = { Book, schemas };
+module.exports = {Book};
