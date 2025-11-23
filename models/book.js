203d7f3d1a067710f10c649e4963eeec60cf0910
grossco <30566134+Allovallo@users.noreply.github.com>
2025-11-23 13:55:12 +0200

    schema updated

--- models/book.js
+++ models/book.js
@@ -35,21 +35,41 @@
 
 // module.exports = { Book, schemas };
 
-const { required } = require("joi");
+// const { required } = require("joi");
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const genreList = ["fantasy", "romantic"];
+const dateRegexp = /^\d{2}-\d{2}-\d{4}$/;
+
+const { handleMongooseError } = require("../helpers");
 
 const bookSchema = new Schema(
   {
     title: { type: String, required: true },
     author: { type: String, required: true },
     favorite: { type: Boolean, default: false },
-    genre: { type: String, enum: ["fantasy", "romantic"], required: true },
+    genre: { type: String, enum: genreList, required: true },
     // 16-10-2009
-    date: { type: String, match: /^\d{2}-\d{2}-\d{4}$/, required: true },
+    date: { type: String, match: dateRegexp, required: true },
   },
   { versionKey: false, timestamps: true }
 );
 
+bookSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+  favorite: Joi.boolean(),
+  genre: Joi.string()
+    .valid(...genreList)
+    .required(),
+  date: Joi.string().pattern(dateRegexp).required(),
+});
+
+const schemas = { addSchema };
+
 const Book = model("book", bookSchema);
 
-module.exports = { Book };
+module.exports = { Book, schemas };
