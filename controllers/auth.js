b7b67a1d0d2411c1731f3554b93c14e3fa1cfbd2
grossco <30566134+Allovallo@users.noreply.github.com>
2025-12-01 16:18:39 +0200

    files updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,11 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -38,9 +41,11 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password invalid");
   }
 
-  const token = "fdshffsdgh.e4rq2tas.dr13541";
+  const payload = { id: user._id, };
 
-  res.token({ token, });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.json({ token });
 };
 
 module.exports = {
