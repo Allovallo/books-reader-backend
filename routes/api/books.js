5181517c84a161f629aa6ccef8b7dd4e0cd7b405
grossco <30566134+Allovallo@users.noreply.github.com>
2025-12-12 15:54:05 +0200

    files updated

--- routes/api/books.js
+++ routes/api/books.js
@@ -5,27 +5,40 @@ const ctrl = require('../../controllers/books');
 const {
   validateBody,
   isValidId,
+  authenticate,
 } = require('../../middlewares');
 
 const { schemas } = require('../../models/book');
 
 const router = express.Router();
 
-router.get('/', ctrl.getAll);
-router.get('/:id', isValidId, ctrl.getById);
-router.post('/', validateBody(schemas.addSchema), ctrl.add);
+router.get('/', authenticate, ctrl.getAll);
+router.get('/:id', authenticate, isValidId, ctrl.getById);
+router.post(
+  '/',
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.add,
+);
 router.put(
   '/:id',
   isValidId,
+  authenticate,
   validateBody(schemas.addSchema),
   ctrl.updateById,
 );
 router.patch(
   '/:id/favorite',
   isValidId,
+  authenticate,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite,
 );
-router.delete('/:id', isValidId, ctrl.deleteById);
+router.delete(
+  '/:id',
+  authenticate,
+  isValidId,
+  ctrl.deleteById,
+);
 
 module.exports = router;
